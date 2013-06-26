f300dba284113b59bea007a5fdc64100d7854650
Charles Dang <exodia339@gmail.com>
2013-06-27 05:22:14 +1100

    Added a help topic about spellcasting

--- lua/data/help.lua
+++ lua/data/help.lua
@@ -11,5 +11,15 @@ Tips:
 
 NOTE: A game screen resolution of 800x600 or greater is recommended. Some sequences make use of floating labels, halos, and standing unit animations, so you might want to make sure these options are enabled under Preferences → Display.]]
 	};
+	{	name = "Spellcasting", text = _ [[
+Units with magical abilities (such as Elynia and Niryone) are able to cast spells. Each has a list of spells they have leared, accessible from the "Spells" right click menu.
+
+To cast a spell:
+• Right click on any unit who has learned spells and select the “Spells” option.
+• Select a spell and click “Cast Spell”. This will create an overlay over any valid potential targets for that spell
+• Right click on any of these highlighted hexes and select the “Cast (spellname) Spell” option.
+
+NOTE: Some spells may have limited usage or cooldown time, so be sure to use them wisely.]]
+	};
 }
 
