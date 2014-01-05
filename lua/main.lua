1127aa98f540895fb2df434bb0a3767d400e99a7
8573 <8573dd@gmail.com>
2014-01-05 01:23:24 +0000

    Check for more incongruities with lp8.
    In `lua/main.lua`, check for incongruities with lp8 before attempting to
    load lp8 modules.
    
    First, check that the global variable `lp8` exists.
    
    If it does, check that it is a table, and that its value equals that of
    the global variable `namespace_of_8680s_Lua_Pack`.

--- lua/main.lua
+++ lua/main.lua
@@ -6,12 +6,23 @@ items = wesnoth.require 'lua/wml/items.lua'
 T = helper.set_wml_tag_metatable {}
 --debug_utils = wesnoth.require '~add-ons/Wesnoth_Lua_Pack/debug_utils.lua'
 
-for _, module in ipairs {
-	'wml';
-	'modifications';
-	'table';
-} do
-	lp8.import(module)
+if lp8 ~= nil then
+	if type(lp8) ~= 'table' or lp8 ~= namespace_of_8680s_Lua_Pack then
+		local function dbgstr(x)
+			return ('%s(%s)'):format(type(x), tostring(x))
+		end
+		error(("lp8 is %s; namespace_of_8680s_Lua_Pack is %s"):
+			format(dbgstr(lp8),
+				dbgstr(namespace_of_8680s_Lua_Pack)))
+	end
+
+	for _, module in ipairs {
+		'wml';
+		'modifications';
+		'table';
+	} do
+		lp8.import(module)
+	end
 end
 
 function nxrequire(script)
