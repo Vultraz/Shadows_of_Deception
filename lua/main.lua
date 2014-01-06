f64333bca6ce0de6d827176cbfe0a050eaece982
8573 <8573dd@gmail.com>
2014-01-06 04:32:22 +0000

    Add friendly error message for missing lp8.
    Add a friendly error message for when NX is run without an lp8 being
    available.
    
    After the message is shown, the scenario is ended in defeat.
    
    At least, I hope it’s friendlier than Lua spewing a backtrace.

--- lua/main.lua
+++ lua/main.lua
@@ -23,6 +23,22 @@ if lp8 ~= nil then
 	} do
 		lp8.import(module)
 	end
+else
+	wml_actions.event {
+		name = 'start';
+		{'message', {
+			speaker = 'narrator';
+			caption = _ "Error: 8680’s Lua Pack is missing";
+			message = tostring(
+_ "You appear to not have 8680’s Lua Pack installed. 8680’s Lua Pack is required for this campaign to run."
+				) .. "\n\n" .. tostring(nxconfig.git and
+_ "Seeing as you are running this campaign from a Git repository, you should read and follow the <i>readme.md</i> file, which can also be found at &lt;https://github.com/Vultraz/NX-RPG#readme&gt;."
+					or
+_ "Please install 8680’s Lua Pack from Battle for Wesnoth’s official add-ons servers, via Battle for Wesnoth’s built-in add-ons manager, which can be accessed via the “Add-ons Manager” button in the main menu."
+				);
+		}};
+		{'endlevel', {result = 'defeat'}};
+	}
 end
 
 function nxrequire(script)
