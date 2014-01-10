301b10cd6c9b99a95a2e4d3883863b563e6f265f
8573 <8573dd@gmail.com>
2014-01-10 03:16:57 +0000

    Split global variable init from `lua/main.lua`.
    Split global variable initialization out from `lua/main.lua` to a new
    `lua/env.lua` file, which is run from `lua/_main.cfg`.

--- lua/main.lua
+++ lua/main.lua
@@ -1,11 +1,4 @@
 
-_ = wesnoth.textdomain 'wesnoth-NX-RPG'
-wml_actions = wesnoth.wml_actions
-helper = wesnoth.require 'lua/helper.lua'
-items = wesnoth.require 'lua/wml/items.lua'
-T = helper.set_wml_tag_metatable {}
---debug_utils = wesnoth.require '~add-ons/Wesnoth_Lua_Pack/debug_utils.lua'
-
 function nxrequire(script)
 	return wesnoth.require('~add-ons/NX-RPG/lua/' .. script .. '.lua')
 end
