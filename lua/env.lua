06c3b59f0670c6aa4e78bc8db15a95c7ebeb6c5b
Charles Dang <exodia339@gmail.com>
2014-01-10 18:45:08 +1100

    Concatenated separate files into single file for easier reading
    The config population is still commented out

--- lua/env.lua
+++
@@ -1,7 +0,0 @@
-
-_ = wesnoth.textdomain 'wesnoth-NX-RPG'
-wml_actions = wesnoth.wml_actions
-helper = wesnoth.require 'lua/helper.lua'
-items = wesnoth.require 'lua/wml/items.lua'
-T = helper.set_wml_tag_metatable {}
---debug_utils = wesnoth.require '~add-ons/Wesnoth_Lua_Pack/debug_utils.lua'
