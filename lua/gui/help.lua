78a317a7e84ead4f1bce7c85fda3f1670f48973a
Charles Dang <exodia339@gmail.com>
2014-01-28 21:21:01 +1100

    Applies pango markup to help dialog and topics

--- lua/gui/help.lua
+++ lua/gui/help.lua
@@ -22,10 +22,11 @@ local function preshow()
 	page_count = #data
 
 	for i, h in ipairs(data) do
-		wesnoth.set_dialog_value(
-			h.name, "topic_list", i, "topic_list_name")
-		wesnoth.set_dialog_value(
-			h.text, "help_text_pages", i, "topic_text")
+		wesnoth.set_dialog_value(h.name, "topic_list", i, "topic_list_name")
+		wesnoth.set_dialog_markup(true,  "topic_list", i, "topic_list_name")
+
+		wesnoth.set_dialog_value(h.text, "help_text_pages", i, "topic_text")
+		wesnoth.set_dialog_markup(true,  "help_text_pages", i, "topic_text")
 	end
 
 	wesnoth.set_dialog_callback(select_topic, "topic_list")
