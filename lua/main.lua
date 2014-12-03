aeed8938f8e428c9b4b56bc22d02f6ceb86a1172
Charles Dang <exodia339@gmail.com>
2014-12-03 17:59:08 +1100

    Config update
    * Dropped positive messages for music pack
    * Dropped music status in stortext since it's in campaign menu
    * Updated descrip footer to include basic min Wesnoth version info and feedback link (looks like AtS's now)
    * Dropped unnecessary defines and the empty nxconfig lua table

--- lua/main.lua
+++ lua/main.lua
@@ -7,16 +7,6 @@ items = wesnoth.require 'lua/wml/items.lua'
 T = helper.set_wml_tag_metatable {}
 --debug_utils = wesnoth.require '~add-ons/Wesnoth_Lua_Pack/debug_utils.lua'
 
-nxconfig = lp8.new_table()
-
--- nxconfig.name = '{NX_NAME}'
-
--- #ifdef NX_IS_GIT
--- nxconfig.git = lp8.new_table()
--- nxconfig.git.head = '{NX_GIT_HEAD}'
--- nxconfig.git.branch = nxconfig.git.head:match '^ref: refs/heads/(.+)'
--- #endif
-
 function nxrequire(script)
 	return wesnoth.require('~add-ons/NX-RPG/lua/' .. script .. '.lua')
 end
