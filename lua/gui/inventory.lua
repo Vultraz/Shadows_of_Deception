e76ce4383a00a8ee3db5ff8b2cf76a88b0365e1b
Charles Dang <exodia339@gmail.com>
2015-04-26 15:57:29 +1100

    Minor markup simplification

--- lua/gui/inventory.lua
+++ lua/gui/inventory.lua
@@ -54,8 +54,8 @@ function wml_actions.show_inventory(cfg)
 				wesnoth.set_dialog_value(item.image, "inventory_list", i, "list_image")
 			end
 
-			wesnoth.set_dialog_value("<span size='large'>" .. item.name .. "</span>", "inventory_list", i, "list_name")
-			wesnoth.set_dialog_markup(true,                                           "inventory_list", i, "list_name")
+			wesnoth.set_dialog_value("<big>" .. item.name .. "</big>", "inventory_list", i, "list_name")
+			wesnoth.set_dialog_markup(true,                            "inventory_list", i, "list_name")
 
 			wesnoth.set_dialog_value(item.name, "details_pages", i, "details_name")
 			wesnoth.set_dialog_value(item.quantity or 1, "inventory_list", i, "list_quantity")
