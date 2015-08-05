d4aadcf63a936283c5ddc01a7a6cc9324bf5f22c
Charles Dang <exodia339@gmail.com>
2015-08-05 18:04:09 +1100

    Keep help topics in alphabetical order

--- lua/data/help.lua
+++ lua/data/help.lua
@@ -11,16 +11,6 @@ Many elements of this campaign differ from standard Wesnoth. If you aren’t sur
  
 <b>NOTE:</b> A game screen resolution of 800x600 or greater is recommended. Additionally, some sequences make use of floating labels, halos, and standing unit animations, so you might want to make sure these options are enabled under Preferences → Display.]]
 	};
-	{ name = _ "Spellcasting", text = _ [[
-Units with magical abilities (such as Elynia and Niryone) are able to cast spells. Each has a list of spells they have learned, accessible from the <b>Spells</b> right click menu.
- 
-<big>To cast a spell:</big>
-• Right click on any unit who has learned spells and select the <b>Spells</b> option.
-• Select a spell and click <b>Cast Spell</b>. This will create an overlay over any valid potential targets for that spell
-• Right click on any of these highlighted hexes and select the <b>Cast Spell: (spell name)</b> option.
- 
-<b>NOTE:</b> Some spells may have limited usage or cooldown time, so be sure to use them wisely.]]
-	};
 	{ name = _ "Items", text = _ [[
 During your playthrough, you may encounter certain items you can pick up and add to characters’ inventories. To access a unit’s inventory, right click on them and select the <b>Inventory</b> option. You will be presented with a list of items that unit is carrying. Each character’s weapons, such as swords or staves, will be also be appear as items in the invetory.
  
@@ -30,5 +20,15 @@ When you pick up an item from the map, you can do two things:
 • <b>Take:</b> Add the item to the relevant character’s inventory for later use.
  
 Equippable items will henceforth appear in your inventory, even if you equip them immediately. They can be equipped or unequipped from there. Single-use items will also appear in your inventory, provided you did not use them when you picked them up.]]
+	};
+	{ name = _ "Spellcasting", text = _ [[
+Units with magical abilities (such as Elynia and Niryone) are able to cast spells. Each has a list of spells they have learned, accessible from the <b>Spells</b> right click menu.
+ 
+<big>To cast a spell:</big>
+• Right click on any unit who has learned spells and select the <b>Spells</b> option.
+• Select a spell and click <b>Cast Spell</b>. This will create an overlay over any valid potential targets for that spell
+• Right click on any of these highlighted hexes and select the <b>Cast Spell: (spell name)</b> option.
+ 
+<b>NOTE:</b> Some spells may have limited usage or cooldown time, so be sure to use them wisely.]]
 	};
 }
